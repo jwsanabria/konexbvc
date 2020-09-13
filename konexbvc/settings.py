e09efb2024e47cf7229cc1e3b7392a0a21b6b126
John_Sanabria <jwsanabria@gmail.com>
2020-09-13 12:42:17 -0500

    first commit

--- konexbvc/settings.py
+++ konexbvc/settings.py
@@ -54,6 +54,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'konexbvc.urls'
 
 TEMPLATES = [
